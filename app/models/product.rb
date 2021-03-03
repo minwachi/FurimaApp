725d02763aae3f5a04109d3a7d4cd263552c1156
vagrant <vagrant@localhost.localdomain>
2021-03-03 10:34:13 +0000

    set model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+	has_many :images
+  	belongs_to :customer, dependent: :destroy
+  	belongs_to :category, dependent: :destroy
+  	belongs_to :brand, dependent: :destroy
 end
